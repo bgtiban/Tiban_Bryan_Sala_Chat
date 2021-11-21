67a6a60f2abf0cd2e83f00a20e04c84fb8328a4e
Bgtiban <bgtiban@outlook.es>
2021-11-21 13:36:19 +0100

    Mensaje de ayuda en caso de no añadir argumentos.

--- Client/src/client/MainClient.java
+++ Client/src/client/MainClient.java
@@ -2,19 +2,25 @@ package client;
 
 import java.io.IOException;
 
-
 public class MainClient {
 
 	public static void main(String[] args) {
-		try {
-//			ServerSocket server = new ServerSocket(new Socket(args[0], Integer.parseInt(args[1])), Integer.parseInt(args[2]));
-			ServerSocket server = new ServerSocket(new Socket("127.0.0.1", 5555), 1234);
-			Client client = new Client(server);
 
-			new JFClient(client).setVisible(true);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		if (args == null || args.length < 3) {
+			System.out.println(
+					"Argumentos esperados: [0]IP servidor, [1]Puerto servidor, [2]Puerto escucha nuevos clientes servidor");
+		} else
 
+			try {
+				ServerSocket server = new ServerSocket(new Socket(args[0], Integer.parseInt(args[1])),
+						Integer.parseInt(args[2]));
+//			ServerSocket server = new ServerSocket(new Socket("127.0.0.1", 5555), 1234);
+				Client client = new Client(server);
+
+				new JFClient(client).setVisible(true);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	}
+
 }
--- Server/src/server/MainServer.java
+++ Server/src/server/MainServer.java
@@ -7,9 +7,13 @@ public class MainServer {
 		Thread ts = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				System.out.println("Servidor a la escucha...");
-//				new Server(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-				new Server("127.0.0.1", 5555, 1234);
+				if(args == null || args.length < 3) {
+					System.out.println("Argumentos esperados: [0]IP servidor, [1]Puerto servidor, [2]Puerto escucha nuevos clientes servidor");
+				}else {
+					new Server(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+					System.out.println("Servidor a la escucha...");
+//					new Server("127.0.0.1", 5555, 1234);
+				}
 
 			}
 		});
